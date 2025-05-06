9e193c019a2b63bb2f6a7839f1e7d3e42083fd21
maksymmaksym029 <dsw58027@student.dsw.edu.pl>
2025-05-06 15:49:57 +0200

    Create help.sh
    Dodano --help

---
+++ help.sh
@@ -0,0 +1,6 @@
+if [[ "$1" == "--help" ]]; then
+  echo "Dostępne opcje:"
+  echo "--date       Wyświetla aktualną datę"
+  echo "--logs [N]   Tworzy N (domyślnie 100) plików logX.txt"
+  echo "--help       Wyświetla tę pomoc"
+fi
